7024a02762372ede048c930582fb9b7fb4f61d64
Michael McMullen <tutelagesystems@users.noreply.github.com>
2016-11-14 08:58:03 -0500

    Added optional userclass parameter

--- src/Commands/MakeUser.php
+++ src/Commands/MakeUser.php
@@ -14,7 +14,8 @@ class MakeUser extends Command
      */
     protected $signature = 'make:user {email : The email address of the user}
                             {--name= : Will assign the name to the user (Optional)}
-                            {--password=N : Allows you to assign the password (Optional)}';
+                            {--password=N : Allows you to assign the password (Optional)}
+                            {--userclass=\App\User : the user class itself, will use Laravel\'s default when omitted}';
 
     /**
      * The console command description.
@@ -22,6 +23,11 @@ class MakeUser extends Command
      * @var string
      */
     protected $description = 'Create a new user based on the email supplied';
+
+    /**
+     * the user class definition
+     */
+    var $userClass;
 
     /**
      * Create a new command instance.
@@ -40,18 +46,23 @@ class MakeUser extends Command
      */
     public function handle()
     {
+        // Email Address of the User
         $emailAddress = $this->argument('email');
+        // Name of the User (default to use Email Address)
         $name = $this->option('name') ? $this->option('name') : $emailAddress;
+        // Random password of the user, unless prompted
         $password = strtolower($this->option('password')) != 'n' ? $this->secret('Please enter a password') : str_random(8);
+        // Default user class within project
+        $userClass = strlen($this->option('userclass')) ? trim($this->option('userclass')) : '\\App\\User';
+        $userDb = new $userClass;
 
-        if(User::where('email', $emailAddress)->count() > 0) {
+        if($userDb::where('email', $emailAddress)->count() > 0) {
             $this->error('Email Address already exists');
             return;
         }
 
-
         $this->info('Creating user for: '. $emailAddress);
-        User::create([
+        $userDb::create([
             'name'     => $name,
             'email'    => $emailAddress,
             'password' => bcrypt($password),
